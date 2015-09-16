c21c0e98ad240d3e3a866161b0b624bd6e8484a2
turao <lenz.arthur@gmail.com>
2015-09-16 00:47:55 -0300

    confirma suspeitas. setima thread forca segmentation fault

--- src/main.c
+++ src/main.c
@@ -11,9 +11,9 @@
 void* inc(void* param)
 {
 	printf("INC chamado!\t");
-	//int* actualParam = param;
-	//*actualParam += 1;
-	//printf("\t parametro passado: %i\n", *actualParam);
+	int* actualParam = param;
+	*actualParam += 1;
+	printf("\t parametro passado: %i\n", *actualParam);
 
 	//piyield();
 	printThread(getRunningThread());
@@ -47,13 +47,16 @@ int main(int argc, char* argv[])
 	//printf("\n Criando thread... \n");
 	//printf("eitanui %d", *a);
 
+	int* a = (int*) malloc(sizeof(int));
+	int value = 10;
+	a = &value;
 	
-	// picreate(91, &inc, a);
-	// picreate(91, &inc, a);
-	// picreate(91, &inc, a);
-	// picreate(91, &inc, a);
-	// picreate(91, &inc, a);
-	// picreate(91, &inc, a);
+	picreate(91, &inc, a);
+	picreate(91, &inc, a);
+	picreate(91, &inc, a);
+	picreate(91, &inc, a);
+	picreate(91, &inc, a);
+	picreate(91, &inc, a);
 	// picreate(91, &inc, a);
 	// picreate(91, &inc, a);
 	// picreate(91, &inc, a);
@@ -83,14 +86,16 @@ int main(int argc, char* argv[])
 	// picreate(54, &inc, a);
 	// picreate(42, &inc, a);
 	// picreate(15, &inc, a);
-	picreate(90, &ahoy, NULL);
-	picreate(99, &inc, NULL);
-	picreate(96, &inc, NULL);
-	picreate(99, &inc, NULL);
-	picreate(86, &ahoy, NULL);
-	picreate(97, &ahoy, NULL);
-	picreate(86, &ahoy, NULL);
-	picreate(99, &ahoy, NULL);
+	
+	// picreate(90, &ahoy, NULL);
+	// picreate(99, &inc, NULL);
+	// picreate(96, &inc, NULL);
+	// picreate(99, &inc, NULL);
+	// picreate(86, &ahoy, NULL);
+	// picreate(97, &ahoy, NULL);
+	// picreate(86, &ahoy, NULL);
+	// picreate(99, &ahoy, NULL);
+
 	// picreate(100, &ahoy, NULL);
 	// picreate(98, &ahoy, NULL);
 	// picreate(99, &inc, NULL);
