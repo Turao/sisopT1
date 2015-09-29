0b58b0a2e78dd183d202d4501db2340d1d320577
turao <lenz.arthur@gmail.com>
2015-09-29 19:41:03 -0300

    cria teste para criacao de threads

--- testes/create.c
+++ testes/create.c
@@ -1,6 +1,8 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+#include "../include/pithread.h"
+
 void* myFunc()
 {
 	printf("myFunc created!\n");
@@ -10,7 +12,19 @@ void* myFunc()
 int main()
 {
 	int tid = picreate(20, &myFunc, NULL);
-	printf("Thread criada com TID: %i\n", tid);
+	if(tid > 0)	printf("Thread criada com TID: %i\n", tid);
+
+	int tid1 = picreate(-1, &myFunc, NULL);
+	if(tid1 > 0)	printf("Thread criada com TID: %i\n", tid1);
+
+	int tid2 = picreate(0, &myFunc, NULL);
+	if(tid2 > 0)	printf("Thread criada com TID: %i\n", tid2);
+
+	int tid3 = picreate(100, &myFunc, NULL);
+	if(tid3 > 0)	printf("Thread criada com TID: %i\n", tid3);
+
+	int tid4 = picreate(101, &myFunc, NULL);
+	if(tid4 > 0)	printf("Thread criada com TID: %i\n", tid4);
 
 	return 0;
 }
